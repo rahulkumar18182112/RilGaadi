2975c7e7d50c8fb878b0efeec360d54dd0f8b1a6
Rahul kumar <rahulkumar7503617457@gmail.com>
2023-02-04 23:12:20 +0530

    initial commit

---
+++ app/src/main/java/com/example/railgaadi/RunningStatus.java
@@ -0,0 +1,14 @@
+package com.example.railgaadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunningStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_status);
+    }
+}
